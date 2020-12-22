c08118ad694e6f7e18b1e655a795977fcf73070b
ksanbal <dev.ksanbal@gmail.com>
2020-12-22 12:11:32 +0900

    upload new restAPI

---
+++ Back/RestAPI/login/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .serializers import PostSerializer
+from .models import Post
+# Create your views here.
+class PostViewset(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
