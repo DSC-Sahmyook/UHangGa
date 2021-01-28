f261dbbcf27cc31df4c0b197f34c396f0ec39fff
ksanbal <dev.ksanbal@gmail.com>
2021-01-29 01:28:36 +0900

    AWS DB 연동코드 작성했습니다. pip install psycopg2 또는 psycopg2-binary 실행해주세요!

--- Back/RestAPI/mbti/settings.py
+++ Back/RestAPI/mbti/settings.py
@@ -84,12 +84,25 @@ WSGI_APPLICATION = 'mbti.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# 테스트용 AWS RDS PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'dsc_uhg',
+        'PASSWORD': 'dscuhg2021',
+        'HOST': 'uhg-postgresql.crxn9cgqjsuv.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
