013747032596e55c2d27a6e2d1967936fe1165c9
ksanbal <dev.ksanbal@gmail.com>
2021-03-30 05:58:13 +0900

    listPage 테스팅

--- Front/APP/dsc_uhangga/uhangga/lib/main.dart
+++ Front/APP/dsc_uhangga/uhangga/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:uhangga/page_mainnew.dart';
 
+import 'Dog_Registration_page/page_dog_list.dart';
+
 // final String address = 'https://dsc-uhg-306513.du.r.appspot.com'; // 주소 받아오는 곳
-final String address = 'https://df8eaee0ba5d.ngrok.io';
+final String address = 'https://c7bb53b6d253.ngrok.io';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
