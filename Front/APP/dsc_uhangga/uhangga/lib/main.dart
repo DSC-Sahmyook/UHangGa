659a4b6ea31516d6799c5b0ce9beb17d905d5bef
ksanbal <dev.ksanbal@gmail.com>
2021-03-30 21:07:18 +0900

    합병 기모띠

--- Front/APP/dsc_uhangga/uhangga/lib/main.dart
+++ Front/APP/dsc_uhangga/uhangga/lib/main.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:uhangga/page_mainnew.dart';
 
 final String address = 'https://dsc-uhg-306513.du.r.appspot.com'; // 주소 받아오는 곳
-<<<<<<< HEAD
-=======
-
->>>>>>> 5a49216bb80eeecd03c6cc75d1b3bbafd42ddba5
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
